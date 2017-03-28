77e4dbbb3cf72df37a9d8dd7153971b6bd87e90b
NessCollege <nesscollege2017@gmail.com>
2017-03-28 19:37:49 +0300

    stepper

--- app/src/main/java/ness/edu/events/MainActivity.java
+++ app/src/main/java/ness/edu/events/MainActivity.java
@@ -5,30 +5,40 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     //define properties:
+    //Button btnPlus;
+    //Button btnMinus;
+    private TextView tvResult;
     //in activities / controllers the properties are the controls.
-    private EditText etMessage;
-    private Button btnSend;
+    private int counter = 0;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        btnSend = (Button) findViewById(R.id.btnSend);
-        etMessage = (EditText) findViewById(R.id.etMessage);
+        //findViewById.
+        tvResult = (TextView) findViewById(R.id.tvResult);
+        setTvResult();
     }
 
-    public void btnClicked(View view) {
-        String name = "Moshe";
-        name += "Cohen";
+    private void setTvResult() {
+        tvResult.setText(String.valueOf(counter));
+    }
 
-        String text = etMessage.getText().toString();
-        Toast.makeText(this, text, Toast.LENGTH_SHORT).show();
+
+    public void btnMinusClicked(View view) {
+        counter--;
+        setTvResult();
+    }
+
+    public void btnPlusClicked(View view) {
+        counter++;
+        setTvResult();
     }
 }
