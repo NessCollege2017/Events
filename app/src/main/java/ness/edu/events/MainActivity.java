1d0d2ba305b197ecee6e7a50d96c93f87ad7cc70
NessCollege <nesscollege2017@gmail.com>
2017-03-28 20:26:11 +0300

    TempConverter

--- app/src/main/java/ness/edu/events/MainActivity.java
+++ app/src/main/java/ness/edu/events/MainActivity.java
@@ -5,40 +5,42 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     //define properties:
-    //Button btnPlus;
-    //Button btnMinus;
-    private TextView tvResult;
-    //in activities / controllers the properties are the controls.
-    private int counter = 0;
-
-
+    RadioGroup rgTemperature;
+    TextView tvResult;
+    EditText etTemperature;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        //findViewById.
+        setContentView(R.layout.temp_converter);
+
+        rgTemperature = (RadioGroup) findViewById(R.id.rgTemperature);
         tvResult = (TextView) findViewById(R.id.tvResult);
-        setTvResult();
+        etTemperature = (EditText) findViewById(R.id.etTemperature);
     }
 
-    private void setTvResult() {
-        tvResult.setText(String.valueOf(counter));
-    }
+    public void calculate(View view) {
+        try {
+            int checkedId = rgTemperature.getCheckedRadioButtonId();
 
+            double result = 0;
+            double temp = Double.valueOf(etTemperature.getText().toString());
+            if (checkedId == R.id.radioCelcius){
+                //°C = (°F  -  32)  x  5/9
+                result = (temp - 32) * 5.0/9;
+            }else {
+                //°F = °C  x  9/5 + 32
+                result = temp * 9.0/5 +32;
+            }
+            tvResult.setText(String.valueOf(result));
+        } catch (NumberFormatException e) {
 
-    public void btnMinusClicked(View view) {
-        counter--;
-        setTvResult();
-    }
-
-    public void btnPlusClicked(View view) {
-        counter++;
-        setTvResult();
+        }
     }
 }
